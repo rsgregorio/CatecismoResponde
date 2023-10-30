a39e787c16ef6969b24b6072198ead8752cc7915
Ricardo Gregorio <ricardosgreg@gmail.com>
2023-10-30 13:14:48 -0300

    implementacao_highlight

--- main.go
+++ main.go
@@ -4,47 +4,55 @@ import (
 	"database/sql"
 	"html/template"
 	"net/http"
+	"regexp"
 
 	_ "github.com/lib/pq"
 )
 
-type Result struct {
-	Numero int
-	Texto  string
+type Data struct {
+	Numero string
+	Texto  template.HTML
 }
 
-var tmpl = template.Must(template.ParseFiles("template.html"))
+func highlight(text, query string) template.HTML {
+	re := regexp.MustCompile(`(?i)` + regexp.QuoteMeta(query))
+	highlightedText := re.ReplaceAllString(text, `<mark>$0</mark>`)
+	return template.HTML(highlightedText)
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("template.html"))
+
 	db, err := sql.Open("postgres", "user=postgres password=root dbname=catecismo sslmode=disable")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		panic(err)
+	}
+
+	if r.Method != http.MethodPost {
+		tmpl.Execute(w, nil)
 		return
 	}
-	defer db.Close()
 
-	if r.Method == http.MethodPost {
-		text := r.FormValue("text")
-		rows, err := db.Query("SELECT numero, texto FROM paragrafos WHERE texto ILIKE $1 ORDER BY numero", "%"+text+"%")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer rows.Close()
+	text := r.FormValue("text")
+	data := []Data{}
 
-		var results []Result
-		for rows.Next() {
-			var result Result
-			if err := rows.Scan(&result.Numero, &result.Texto); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			results = append(results, result)
-		}
-		tmpl.Execute(w, results)
-	} else {
-		tmpl.Execute(w, nil)
+	rows, err := db.Query("SELECT numero, texto FROM paragrafos WHERE texto ILIKE $1 ORDER BY numero", "%"+text+"%")
+	if err != nil {
+		panic(err)
 	}
+
+	for rows.Next() {
+		var numero string
+		var texto template.HTML
+		err = rows.Scan(&numero, &texto)
+		if err != nil {
+			panic(err)
+		}
+		texto = highlight(string(texto), text)
+		data = append(data, Data{Numero: numero, Texto: texto})
+	}
+
+	tmpl.Execute(w, data)
 }
 
 func main() {
