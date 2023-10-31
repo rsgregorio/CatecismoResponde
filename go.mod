2a1240eae49d42ac9f3ee826d7c5243cbccfe3b6
Ricardo Gregorio <ricardosgreg@gmail.com>
2023-10-31 13:04:37 -0300

    adiçãoGitIgnore_EnvFile

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.21.0
 
 require github.com/lib/pq v1.10.9
 
-require golang.org/x/text v0.13.0 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,14 +4,29 @@ import (
 	"database/sql"
 	"html/template"
 	"net/http"
+	"os"
 
-	"github.com/rsgregorio/CatecismoResponde/controllers"
-
+	"github.com/joho/godotenv" // Importe a biblioteca godotenv
 	_ "github.com/lib/pq"
+	"github.com/rsgregorio/CatecismoResponde/controllers"
 )
 
+func init() {
+	// Carregue as variáveis de ambiente do arquivo .env
+	if err := godotenv.Load(); err != nil {
+		panic("Erro ao carregar o arquivo .env: " + err.Error())
+	}
+}
+
 func main() {
-	db, err := sql.Open("postgres", "user=postgres password=root dbname=catecismo sslmode=disable")
+
+	connStr := "user=" + os.Getenv("DB_USER") + " " +
+		"password=" + os.Getenv("DB_PASSWORD") + " " +
+		"dbname=" + os.Getenv("DB_NAME") + " " +
+		"host=" + os.Getenv("DB_HOST") + " " +
+		"port=" + os.Getenv("DB_PORT") + " " +
+		"sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		panic(err)
 	}
